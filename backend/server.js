1a258cbff4133c8b4161d33ee111ccd88bf89d50
rvua <rvanore26@gmail.com>
2022-02-24 17:33:41 -0600

    Version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
@@ -7,6 +8,13 @@ const port = 8000;
 const cors = require("cors");
 
 app.use(cors());
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 require("./server/config/mongoose.config");
 
